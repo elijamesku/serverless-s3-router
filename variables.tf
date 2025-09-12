5bfc0974c8bac9e1dc72b201d0ace73b6a056a69
Eli James <ejames@lead.bank>
2025-09-12 12:17:53 -0500

    variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,17 @@ variable "region" {
     type = string
     default = "us-east-2" 
 }
+
+#will be changing default to true to run kms later
+variable "kms_sse" {
+    type = bool
+    default = false 
+}
+
+variable "tags" {
+    type = map(string)
+    default = {
+      Owner = "eli"
+      Purpose = "serverless-s3-router"
+    }
+}
