0a8b72adc28cd6c64feee117069b35d75c8c004c
Eli James <ejames@lead.bank>
2025-09-12 12:19:32 -0500

    terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "project" {
-    type = string
-    default = "s3-router"
+  type    = string
+  default = "s3-router"
 }
 
 variable "region" {
-    type = string
-    default = "us-east-2" 
+  type    = string
+  default = "us-east-2"
 }
 
 #will be changing default to true to run kms later
 variable "kms_sse" {
-    type = bool
-    default = false 
+  type    = bool
+  default = false
 }
 
 variable "tags" {
-    type = map(string)
-    default = {
-      Owner = "eli"
-      Purpose = "serverless-s3-router"
-    }
+  type = map(string)
+  default = {
+    Owner   = "eli"
+    Purpose = "serverless-s3-router"
+  }
 }
