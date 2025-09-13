4da497deb4dd0d07b0abc57349b8c782ec7ad882
Eli James <ejames@lead.bank>
2025-09-13 17:46:13 -0500

    edits

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "processed_bucket" {
 output "archive_bucket" {
   value = aws_s3_bucket.archive.bucket
 }
+
+output "api_base_url" {
+  value = "${aws_apigatewayv2_api.api.api_endpoint}/${aws_apigatewayv2_stage.api_stage.name}"
+}
+
+output "cdn_domain_name" {
+  value = aws_cloudfront_distribution.processed_cdn.domain_name
+}
