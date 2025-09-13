f37aa805872a929574dcf42e00cee662c4bd05d0
Eli James <ejames@lead.bank>
2025-09-13 15:45:20 -0500

    main

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "intake_bucket" {
-    value = aws_s3_bucket.intake.bucket
+  value = aws_s3_bucket.intake.bucket
 }
 
 output "processed_bucket" {
-    value = aws_s3_bucket.processed.bucket
+  value = aws_s3_bucket.processed.bucket
 }
 
 output "archive_bucket" {
-    value = aws_s3_bucket.archive.bucket
+  value = aws_s3_bucket.archive.bucket
 }
